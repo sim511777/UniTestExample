3966db972da4119231e8fa13605bdb6b1ea6a8be
sim511777 <sim51177@gmail.com>
2021-11-15 10:00:51 +0900

    초기 커밋.

---
+++ MathLib/Arith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class Arith
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Sub(int a, int b) => a - b;
+        public static int Mul(int a, int b) => a * b;
+        public static int Div(int a, int b) => a / b;
+    }
+}
---
+++ MathLibTests/ArithTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib.Tests
+{
+    [TestClass()]
+    public class ArithTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.AreEqual(7, MathLib.Arith.Add(3, 4));
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            Assert.AreEqual(-1, MathLib.Arith.Sub(3, 4));
+        }
+
+        [TestMethod()]
+        public void MulTest()
+        {
+            Assert.AreEqual(12, MathLib.Arith.Mul(3, 4));
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            Assert.AreEqual(0, MathLib.Arith.Div(3, 4));
+        }
+    }
+}
